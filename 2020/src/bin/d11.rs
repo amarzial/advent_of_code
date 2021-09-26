185089ab197ae5211c99c197e6706289ed5c7fc9
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2021-09-26 18:27:58 +0200

    2020 d11 - rust

--- 2020/src/bin/d11.rs
+++ 2020/src/bin/d11.rs
@@ -11,10 +11,11 @@ struct Grid {
     count: Grd,
     next: Grd,
     changed: bool,
+    cache: HashMap<usize, [Option<usize>; 8]>,
 }
 
 impl Grid {
-    fn new(grid: Vec<Vec<u8>>) -> Grid {
+    fn new(grid: &Vec<Vec<u8>>) -> Grid {
         let mut g = Grid {
             width: 0,
             height: 0,
@@ -23,6 +24,7 @@ impl Grid {
             count: Grd::new(),
             next: Grd::new(),
             changed: true,
+            cache: HashMap::new(),
         };
 
         g.height = grid.len();
@@ -100,6 +102,59 @@ impl Grid {
         }
     }
 
+    fn find(&self, pos: usize, x_increment: i32, y_increment: i32) -> Option<usize> {
+        let mut y = (pos / self.width) as i32;
+        let mut x = (pos % self.width) as i32;
+        loop {
+            x += x_increment;
+            y += y_increment;
+            if x < 0 || x > (self.width - 1) as i32 || y < 0 || y > (self.height - 1) as i32 {
+                return None;
+            }
+            let cursor = y * self.width as i32 + x;
+            if *self.empty.get(&(cursor as usize)).unwrap_or(&0) == 0 {
+                return Some(cursor as usize);
+            }
+        }
+    }
+
+    fn load(&mut self) {
+        for pos in 0..(self.width * self.height) {
+            if *self.empty.get(&pos).unwrap_or(&0) > 0 {
+                continue;
+            }
+            let mut closest: [Option<usize>; 8] = [None; 8];
+            closest[0] = self.find(pos, -1, -1);
+            closest[1] = self.find(pos, 0, -1);
+            closest[2] = self.find(pos, 1, -1);
+            closest[3] = self.find(pos, -1, 0);
+            closest[4] = self.find(pos, 1, 0);
+            closest[5] = self.find(pos, -1, 1);
+            closest[6] = self.find(pos, 0, 1);
+            closest[7] = self.find(pos, 1, 1);
+            self.cache.insert(pos, closest);
+        }
+    }
+
+    fn fill2(&mut self) {
+        self.count.clear();
+        for k in self.used.keys() {
+            if !self.cache.contains_key(k) {
+                continue;
+            }
+            let closest = self.cache.get(k).unwrap();
+            for item in closest.iter() {
+                match item {
+                    Some(val) => {
+                        self.count
+                            .insert(*val, self.count.get(val).unwrap_or(&0) + 1);
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
     fn run(&mut self) {
         self.fill();
         self.next.clear();
@@ -114,6 +169,29 @@ impl Grid {
                 self.next.insert(pos, 1);
                 self.changed |= current == 0;
             } else if cnt >= 4 {
+                self.next.remove(&pos);
+                self.changed |= current > 0;
+            } else if self.used.contains_key(&pos) {
+                self.next.insert(pos, 1);
+            }
+        }
+        self.used.clone_from(&self.next);
+    }
+
+    fn run2(&mut self) {
+        self.fill2();
+        self.next.clear();
+        self.changed = false;
+        for pos in 0..(self.width * self.height) {
+            if *self.empty.get(&pos).unwrap_or(&0) > 0 {
+                continue;
+            }
+            let current = *self.used.get(&pos).unwrap_or(&0);
+            let cnt = *self.count.get(&pos).unwrap_or(&0);
+            if cnt == 0 {
+                self.next.insert(pos, 1);
+                self.changed |= current == 0;
+            } else if cnt >= 5 {
                 self.next.remove(&pos);
                 self.changed |= current > 0;
             } else if self.used.contains_key(&pos) {
@@ -167,9 +245,11 @@ impl fmt::Display for Grid {
 }
 
 fn main() {
-    let mut grid = Grid::new(utils::read_list_parse("inputs/d11.txt", |str| {
+    let input = utils::read_list_parse("inputs/d11.txt", |str| {
         return str.as_bytes().to_owned();
-    }));
+    });
+    let mut grid = Grid::new(&input);
+
     loop {
         grid.run();
         if !grid.changed {
@@ -177,4 +257,14 @@ fn main() {
         }
     }
     println!("Part 1: {}", grid.count());
+
+    grid = Grid::new(&input);
+    grid.load();
+    loop {
+        grid.run2();
+        if !grid.changed {
+            break;
+        }
+    }
+    println!("Part 2: {}", grid.count());
 }
