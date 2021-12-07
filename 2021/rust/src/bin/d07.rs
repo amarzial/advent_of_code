cb17af8c623d018a266ea23b7405b63caee81ff2
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2021-12-07 19:42:27 +0100

    2021 d07 rust

---
+++ 2021/rust/src/bin/d07.rs
@@ -0,0 +1,33 @@
+use aoc2021::utils;
+
+fn calc_fuel(crabs: &Vec<i32>, target: i32) -> i32 {
+    let mut fuel = 0;
+    for c in crabs.iter() {
+        fuel += i32::abs(*c - target);
+    }
+    return fuel;
+}
+
+fn calc_fuel2(crabs: &Vec<i32>, target: i32) -> i32 {
+    let mut fuel = 0;
+    for c in crabs.iter() {
+        let distance = i32::abs(*c - target);
+        for i in 1..=distance {
+            fuel += i;
+        }
+    }
+    return fuel;
+}
+
+fn main() {
+    let mut input: Vec<i32> = utils::read_line(&utils::get_input(), ",");
+    input.sort();
+
+    let median = input[input.len() / 2 - 1];
+    let p1 = calc_fuel(&input, median);
+    println!("Part 1: {}", p1);
+
+    let avg = input.iter().sum::<i32>() / input.len() as i32;
+    let p2 = calc_fuel2(&input, avg);
+    println!("Part 2: {}", p2);
+}
