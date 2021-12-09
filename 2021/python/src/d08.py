02b6489ba36eaee21b01ee1d2d55aa71bf7e7b11
Pierleni <pierini.m@gmail.com>
2021-12-09 21:21:16 +0100

    2021 d08 python

---
+++ 2021/python/src/d08.py
@@ -0,0 +1,70 @@
+data = open("../input/08.txt", 'r').read().splitlines()
+dgt = [s.split(' | ')[1] for s in data]
+dgt, count = [s.split() for s in dgt], 0
+
+for s in dgt:
+    for k in s:
+        if len(k) in (2, 4, 3, 7):
+            count += 1
+print(f"Part 1: {count}")
+
+# ////////////////// PART 2 //////////////
+
+MAP = {}
+result = 0
+
+def decode(a, b):
+    sum = 0
+    for l in a:
+        if l in b:
+            sum += 1
+    return sum
+
+def wires(signals):
+    for str in signals:
+        if len(str) == 2:
+            MAP[1] = str
+        elif len(str) == 4:
+            MAP[4] = str
+        elif len(str) == 3:
+            MAP[7] = str
+        elif len(str) == 7:
+            MAP[8] = str
+
+    for str in signals:
+        if len(str) == 5:
+            if decode(str, MAP[1]) == 2:        # 3
+                MAP[3] = str
+            elif decode(str, MAP[4]) == 2:      # 2
+                MAP[2] = str
+            else:                               # 5
+                MAP[5] = str
+
+        elif len(str) == 6:
+            if decode(str, MAP[4]) == 4:        # 9
+                MAP[9] = str
+            elif decode(str, MAP[1]) == 2:      # 0
+                MAP[0] = str
+            else:                               # 6
+                MAP[6] = str
+
+
+for line in data:
+    count = ''
+    sgn, dgt = line.split(' | ')
+    wires(sgn.split(' '))
+    for d in dgt.split(' '):
+        for n in range(10):
+            if sorted(d) == sorted(MAP[n]):
+                count += str(n)
+    result += int(count)
+
+print(f"Part 2: {result}")
+
+
+
+
+
+
+
+
