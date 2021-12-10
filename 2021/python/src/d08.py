4c07d593d9292159cb0eb3b38fe7e882f0faef32
Pierleni <pierini.m@gmail.com>
2021-12-10 19:26:02 +0100

    2021 d08 python clnd

--- 2021/python/src/d08.py
+++ 2021/python/src/d08.py
@@ -10,7 +10,7 @@ print(f"Part 1: {count}")
 
 # ////////////////// PART 2 //////////////
 
-MAP = {}
+map = {}
 result = 0
 
 def decode(a, b):
@@ -23,30 +23,30 @@ def decode(a, b):
 def wires(signals):
     for str in signals:
         if len(str) == 2:
-            MAP[1] = str
+            map[1] = str
         elif len(str) == 4:
-            MAP[4] = str
+            map[4] = str
         elif len(str) == 3:
-            MAP[7] = str
+            map[7] = str
         elif len(str) == 7:
-            MAP[8] = str
+            map[8] = str
 
     for str in signals:
         if len(str) == 5:
-            if decode(str, MAP[1]) == 2:        # 3
-                MAP[3] = str
-            elif decode(str, MAP[4]) == 2:      # 2
-                MAP[2] = str
+            if decode(str, map[1]) == 2:        # 3
+                map[3] = str
+            elif decode(str, map[4]) == 2:      # 2
+                map[2] = str
             else:                               # 5
-                MAP[5] = str
+                map[5] = str
 
         elif len(str) == 6:
-            if decode(str, MAP[4]) == 4:        # 9
-                MAP[9] = str
-            elif decode(str, MAP[1]) == 2:      # 0
-                MAP[0] = str
+            if decode(str, map[4]) == 4:        # 9
+                map[9] = str
+            elif decode(str, map[1]) == 2:      # 0
+                map[0] = str
             else:                               # 6
-                MAP[6] = str
+                map[6] = str
 
 
 for line in data:
@@ -55,7 +55,7 @@ for line in data:
     wires(sgn.split(' '))
     for d in dgt.split(' '):
         for n in range(10):
-            if sorted(d) == sorted(MAP[n]):
+            if sorted(d) == sorted(map[n]):
                 count += str(n)
     result += int(count)
 
