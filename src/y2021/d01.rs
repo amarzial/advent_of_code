eb804492a09be7d1bb56c1d17ced8128d025c2c3
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2022-11-20 19:14:45 +0100

    2021 d01

--- src/y2021/d01.rs
+++ src/y2021/d01.rs
@@ -1,6 +1,28 @@
 use crate::day;
+use crate::utils;
 
-pub fn run(_d: &mut day::Day, _which: day::Part) -> bool {
-    _d.part1 = Some(String::from("AAAA"));
+pub fn run(d: &mut day::Day, _which: day::Part) -> bool {
+    let sonar: Vec<u32> = utils::read_list(&d.input);
+
+    let mut last = *sonar.get(0).unwrap();
+    let mut cnt = 0;
+    for depth in sonar.iter() {
+        if *depth > last {
+            cnt += 1;
+        }
+        last = *depth;
+    }
+
+    let mut cnt2 = 0;
+    let mut last2: u32 = sonar.windows(3).next().unwrap().iter().sum();
+    for win in sonar.windows(3).skip(1) {
+        let s: u32 = win.iter().sum();
+        if s > last2 {
+            cnt2 += 1;
+        }
+        last2 = s;
+    }
+    d.part1 = Some(cnt.to_string());
+    d.part2 = Some(cnt2.to_string());
     true
 }
