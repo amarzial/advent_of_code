b0bae7424a06a7cc7d0bf99372cc5d80c809ffaa
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2022-11-21 20:27:32 +0100

    2021 d03

--- src/y2021/d03.rs
+++ src/y2021/d03.rs
@@ -1,5 +1,78 @@
 use crate::day;
 
-pub fn run(_d: &mut day::Day) -> bool {
+use crate::utils;
+
+fn ratings<'a>(list: impl Iterator<Item = &'a Box<String>>, length: usize) -> Vec<i32> {
+    let mut bits = Vec::new();
+    bits.resize(length, 0);
+    for number in list {
+        let s = number.as_bytes();
+        for i in 0..length {
+            if s[i] == b'1' {
+                bits[i] += 1;
+            }
+        }
+    }
+    bits
+}
+
+pub fn run(d: &mut day::Day) -> bool {
+    let list: Vec<Box<String>> = utils::read_list_parse(&d.input, |line| -> Box<String> {
+        return Box::new(String::from(line));
+    });
+    let length = 12;
+    let bits = ratings(list.iter(), length);
+
+    let half = list.len() / 2;
+    let mut gamma = 0;
+    for i in 0..length {
+        gamma <<= 1;
+        gamma |= if bits[i] as usize > half { 1 } else { 0 };
+    }
+    let epsilon = !gamma & (2u32.pow(length as u32) - 1);
+
+    d.set_part_1((gamma * epsilon).to_string());
+
+    //part 2
+    let mut ox = list.clone();
+    let mut i = 0;
+    while ox.len() > 1 {
+        let rate = ratings(ox.iter(), i + 1);
+        let cnt = rate[rate.len() - 1];
+        let compare = if cnt >= ox.len() as i32 - cnt {
+            b'1'
+        } else {
+            b'0'
+        };
+
+        ox = ox
+            .into_iter()
+            .filter(|x| -> bool { (*x).as_bytes()[i] == compare })
+            .collect();
+        i += 1;
+    }
+    let mut co = list.clone();
+    i = 0;
+    while co.len() > 1 {
+        let rate = ratings(co.iter(), i + 1);
+        let cnt = rate[rate.len() - 1];
+        let compare = if cnt < co.len() as i32 - cnt {
+            b'1'
+        } else {
+            b'0'
+        };
+
+        co = co
+            .into_iter()
+            .filter(|x| -> bool { (*x).as_bytes()[i] == compare })
+            .collect();
+        i += 1;
+    }
+
+    d.set_part_2(
+        (i32::from_str_radix(&ox[0], 2).unwrap() * i32::from_str_radix(&co[0], 2).unwrap())
+            .to_string(),
+    );
+
     true
 }
