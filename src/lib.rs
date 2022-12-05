eefbb9d410420e344f689ee4fcec065a99439338
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2022-12-05 17:03:03 +0100

    solve with generic input

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub mod utils;
 #[macro_export]
 macro_rules! solve {
     ($part: expr, $function:ident, $input: expr) => {{
-        fn print_result<T: std::fmt::Display>(func: impl FnOnce(&str) -> Option<T>, input: &str) {
+        fn print_result<T: std::fmt::Display, P>(func: impl FnOnce(P) -> Option<T>, input: P) {
             let timer = std::time::Instant::now();
             let result = func(input);
             let elapsed = timer.elapsed();
