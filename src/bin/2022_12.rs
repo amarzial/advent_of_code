32adbceb016408b2e5eab13aef4649cd03baa2e4
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2022-12-12 09:05:37 +0100

    2022 12

---
+++ src/bin/2022_12.rs
@@ -0,0 +1,109 @@
+use aoc::helpers::Coordinate;
+
+fn part_one(input: &str) -> Option<usize> {
+    let grid: Vec<Vec<char>> = aoc::utils::read_list_parse(input, |l| l.chars().collect());
+
+    let mut start = Coordinate::new(0 as i32, 0 as i32);
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] == 'E' {
+                start = Coordinate::new(x as i32, y as i32);
+                break;
+            }
+        }
+    }
+
+    let res = pathfinding::directed::dijkstra::dijkstra(
+        &start,
+        |s| {
+            s.neighbors()
+                .into_iter()
+                .filter(|n| {
+                    if n.x < 0 || n.x >= grid[0].len() as i32 || n.y < 0 || n.y >= grid.len() as i32
+                    {
+                        return false;
+                    }
+
+                    let mut from = grid[s.y as usize][s.x as usize];
+                    let mut to = grid[n.y as usize][n.x as usize];
+                    if to == 'S' {
+                        to = 'a';
+                    };
+                    if from == 'E' {
+                        from = 'z';
+                    };
+                    (from <= to) || (from as u8 - to as u8 == 1)
+                })
+                .map(|n| (n, 1))
+                .collect::<Vec<(Coordinate<i32>, usize)>>()
+        },
+        |n| grid[n.y as usize][n.x as usize] == 'S',
+    );
+    Some(res.unwrap().1)
+}
+
+fn part_two(input: &str) -> Option<usize> {
+    let grid: Vec<Vec<char>> = aoc::utils::read_list_parse(input, |l| l.chars().collect());
+
+    let mut start = Coordinate::new(0 as i32, 0 as i32);
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] == 'E' {
+                start = Coordinate::new(x as i32, y as i32);
+                break;
+            }
+        }
+    }
+
+    let res = pathfinding::directed::dijkstra::dijkstra(
+        &start,
+        |s| {
+            s.neighbors()
+                .into_iter()
+                .filter(|n| {
+                    if n.x < 0 || n.x >= grid[0].len() as i32 || n.y < 0 || n.y >= grid.len() as i32
+                    {
+                        return false;
+                    }
+
+                    let mut from = grid[s.y as usize][s.x as usize];
+                    let mut to = grid[n.y as usize][n.x as usize];
+                    if to == 'S' {
+                        to = 'a';
+                    };
+                    if from == 'E' {
+                        from = 'z';
+                    };
+                    (from <= to) || (from as u8 - to as u8 == 1)
+                })
+                .map(|n| (n, 1))
+                .collect::<Vec<(Coordinate<i32>, usize)>>()
+        },
+        |n| grid[n.y as usize][n.x as usize] == 'a' || grid[n.y as usize][n.x as usize] == 'S',
+    );
+    Some(res.unwrap().1)
+}
+
+fn main() {
+    let input = aoc::utils::load_input("inputs", 2022, 12);
+    aoc::solve!(1, part_one, &input);
+    aoc::solve!(2, part_two, &input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part_one() {
+        let input = aoc::utils::load_input("examples", 2022, 12);
+        assert_eq!(part_one(&input), Some(31));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = aoc::utils::load_input("examples", 2022, 12);
+        assert_eq!(part_two(&input), Some(29));
+    }
+}
