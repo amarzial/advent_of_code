10a9cb26398322c35bef76ab46faebb5cb276b07
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2022-12-15 19:02:30 +0100

    2022 15 part 2

--- src/bin/2022_15.rs
+++ src/bin/2022_15.rs
@@ -23,7 +23,8 @@ fn parse_line(l: &str) -> (Coord, Coord) {
 fn part_one(input: &str) -> Option<usize> {
     let points = aoc::utils::read_list_parse(input, parse_line);
 
-    let row_number = 2e6 as i32;
+    let row_number = if cfg!(test) { 10 } else { 2e6 as i32 };
+
     let mut ranges = Vec::new();
     let mut beacons = HashSet::new();
 
@@ -64,21 +65,48 @@ fn part_one(input: &str) -> Option<usize> {
     Some(out - beacons.len())
 }
 
-fn part_two(input: &str) -> Option<String> {
-    // let points = aoc::utils::read_list_parse(input, parse_line);
+fn part_two(input: &str) -> Option<usize> {
+    let points = aoc::utils::read_list_parse(input, parse_line);
 
-    // let row_number = 2000000;
-    // let mut row = HashMap::new();
+    let upper_bound = if cfg!(test) { 20 } else { 4e6 as i32 };
 
-    // for pair in points.iter() {
-    //     if pair.0.y == row_number {
-    //         row.insert(pair.0, 1);
-    //     }
-    //     if pair.1.y == row_number {
-    //         row.insert(pair.1, 0);
-    //     }
-    // }
-    None
+    for row_number in 0_i32..upper_bound {
+        // println!("Row: {}", row_number);
+        let mut ranges = Vec::new();
+
+        for pair in points.iter() {
+            let dist = pair.0.manhattan(&pair.1);
+            let closest_on_row = Coord::new(pair.0.x, row_number);
+            if dist >= pair.0.manhattan(&closest_on_row) {
+                let dist_excess = dist - pair.0.manhattan(&closest_on_row);
+                let low = 0.max(closest_on_row.x - dist_excess);
+                let high = upper_bound.min(closest_on_row.x + dist_excess + 1);
+                ranges.push(low..high);
+            }
+        }
+
+        ranges.sort_by(|a, b| a.start.cmp(&b.start));
+
+        let mut result = Vec::new();
+        result.push(ranges[0].clone());
+
+        for i in 1..ranges.len() {
+            let current = ranges[i].clone();
+            let last = result.len() - 1;
+            if result[last].end >= current.start {
+                result[last] = result[last].start..(result[last].end.max(current.end));
+            } else {
+                result.push(current);
+            }
+        }
+        // println!("ranges: {:?}", result);
+
+        let out = result.iter().fold(0, |tot, current| tot + current.len());
+        if out < upper_bound as usize {
+            return Some(result[0].end as usize * 4e6 as usize + row_number as usize);
+        }
+    }
+    Some(0)
 }
 
 fn main() {
@@ -99,6 +127,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = aoc::utils::load_input("examples", 2022, 15);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(56000011));
     }
 }
