9b4b2aef96dbf5b4697c56f526f0e216c4c7eb79
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2022-12-15 17:07:47 +0100

    2022 15 Optimization

--- src/bin/2022_15.rs
+++ src/bin/2022_15.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use aoc::helpers::Coordinate;
 
@@ -20,49 +20,64 @@ fn parse_line(l: &str) -> (Coord, Coord) {
     )
 }
 
-fn part_one(input: &str) -> Option<i32> {
+fn part_one(input: &str) -> Option<usize> {
     let points = aoc::utils::read_list_parse(input, parse_line);
 
-    let row_number = 2000000;
-    let mut row = HashMap::new();
+    let row_number = 2e6 as i32;
+    let mut ranges = Vec::new();
+    let mut beacons = HashSet::new();
 
     for pair in points.iter() {
         if pair.0.y == row_number {
-            row.insert(pair.0, 1);
+            ranges.push(pair.0.x..pair.0.x + 1);
         }
         if pair.1.y == row_number {
-            row.insert(pair.1, 0);
+            beacons.insert(pair.1);
         }
     }
 
     for pair in points.iter() {
         let dist = pair.0.manhattan(&pair.1);
         let closest_on_row = Coord::new(pair.0.x, row_number);
-        let mut cursor = closest_on_row;
         if dist >= pair.0.manhattan(&closest_on_row) {
             let dist_excess = dist - pair.0.manhattan(&closest_on_row);
-            for i in (closest_on_row.x - dist_excess)..closest_on_row.x {
-                cursor.x = i;
-                if !row.contains_key(&cursor) {
-                    row.insert(cursor, 1);
-                }
-            }
-            cursor = closest_on_row;
-            for i in closest_on_row.x..(closest_on_row.x + dist_excess + 1) {
-                cursor.x = i;
-                if !row.contains_key(&cursor) {
-                    row.insert(cursor, 1);
-                }
-            }
+            ranges.push(closest_on_row.x - dist_excess..closest_on_row.x + dist_excess + 1);
         }
     }
 
-    let out = row.values().sum();
+    ranges.sort_by(|a, b| a.start.cmp(&b.start));
 
-    Some(out)
+    let mut result = Vec::new();
+    result.push(ranges[0].clone());
+
+    for i in 1..ranges.len() {
+        let current = ranges[i].clone();
+        let last = result.len() - 1;
+        if result[last].end >= current.start {
+            result[last] = result[last].start..(result[last].end.max(current.end));
+        } else {
+            result.push(current);
+        }
+    }
+
+    let out = result.iter().fold(0, |tot, current| tot + current.len());
+    Some(out - beacons.len())
 }
 
 fn part_two(input: &str) -> Option<String> {
+    // let points = aoc::utils::read_list_parse(input, parse_line);
+
+    // let row_number = 2000000;
+    // let mut row = HashMap::new();
+
+    // for pair in points.iter() {
+    //     if pair.0.y == row_number {
+    //         row.insert(pair.0, 1);
+    //     }
+    //     if pair.1.y == row_number {
+    //         row.insert(pair.1, 0);
+    //     }
+    // }
     None
 }
 
