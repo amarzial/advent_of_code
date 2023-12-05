becd6db6472b181b6298d07b3f80cfc93dee3f6f
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2023-12-05 23:44:54 +0100

    2023 d05 part2

--- src/bin/2023_05.rs
+++ src/bin/2023_05.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
+use std::vec;
 
 type Int = u64;
 type Mapping = Vec<(Int, Int, Int)>;
+type Range = (Int, Int);
 
 fn parser(input: &str) -> (Vec<Int>, Vec<Mapping>) {
     let lines: Vec<String> = aoc::utils::read_list(input);
@@ -41,6 +42,7 @@ fn parser(input: &str) -> (Vec<Int>, Vec<Mapping>) {
                 }
             }
         }
+        mapping.sort_by(|a, b| Ord::cmp(&a.1, &b.1));
         maps.push(mapping);
     }
 
@@ -71,8 +73,58 @@ fn part_one(input: &str) -> Option<Int> {
     Some(min)
 }
 
-fn part_two(input: &str) -> Option<String> {
-    None
+fn lookup2(mut range: Range, map: &Mapping) -> Vec<Range> {
+    let mut result = vec![];
+    for r in map {
+        if range.1 == 0 {
+            break;
+        }
+        if range.0 + range.1 > r.1 && r.1 + r.2 > range.0 {
+            if range.0 < r.1 {
+                let diff = r.1 - range.0;
+                result.push((range.0, diff));
+                range.0 += diff;
+                range.1 -= diff;
+            }
+
+            let len = Int::min(range.0 + range.1, r.1 + r.2) - range.0;
+            result.push((range.0 + r.0 - r.1, len));
+            range.0 += len;
+            range.1 -= len;
+        } else {
+            if r.1 > range.0 {
+                break;
+            }
+        }
+    }
+    if range.1 > 0 {
+        result.push(range);
+    }
+    return result;
+}
+
+fn location2(seed_range: Range, maps: &Vec<Mapping>) -> Vec<Range> {
+    let mut current = vec![seed_range];
+    for map in maps {
+        let mut results = vec![];
+        for range in current {
+            results.extend(lookup2(range, map));
+        }
+        current = results;
+    }
+    return current;
+}
+
+fn part_two(input: &str) -> Option<Int> {
+    let (seeds, maps) = parser(input);
+
+    let mut results = vec![];
+
+    for range in seeds.chunks(2) {
+        results.extend(location2((range[0], range[1]), &maps));
+    }
+
+    Some(results.iter().map(|r| r.0).min().unwrap())
 }
 
 fn main() {
@@ -93,6 +145,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = aoc::utils::load_input("examples", 2023, 05);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(46));
     }
 }
